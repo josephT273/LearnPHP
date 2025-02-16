b0771769d23cd11c9c7016c389001569f85c9b52
josepht273 <josephtadessem@gmail.com>
2025-02-16 11:12:41 +0300

    Refactor filtering logic in FilteringAndFunction.php and add releaseYear to book entries

--- FilteringAndFunction.php
+++ FilteringAndFunction.php
@@ -12,38 +12,46 @@ $books = [
         'name' => 'The Great Gatsby',
         'author' => 'F. Scott Fitzgerald',
         'purchaseUrl' => 'https://example.com/the-great-gatsby',
+        'releaseYear' => 1925,
     ],
     [
         'name' => 'To Kill a Mockingbird',
         'author' => 'Harper Lee',
         'purchaseUrl' => 'https://example.com/to-kill-a-mockingbird',
+        'releaseYear' => 1960,
     ],
     [
         'name' => '1984',
         'author' => 'George Orwell',
         'purchaseUrl' => 'https://example.com/1984',
+        'releaseYear' => 1949,
     ],
     [
         'name' => 'Pride and Prejudice',
         'author' => 'Jane Austen',
         'purchaseUrl' => 'https://example.com/pride-and-prejudice',
+        'releaseYear' => 1813,
     ],
 ];
 
-function filterByAuthor($books, $author){
-    $filteredBooks = [];
-    foreach($books as $book){
-        if($book['author'] === $author){
-            $filteredBooks[] = $book;
+// User generated function
+function filter($items, $function){
+    $filteredItems = [];
+    foreach($items as $item){
+        if($function($item)){
+            $filteredItems[] = $item;
         }
     }
-    return $filteredBooks;
+    return $filteredItems;
 }
 
+$filteredBooks = array_filter($books,  function ($book) {
+    return $book['releaseYear'] < 1950;
+});
 ?>
 <h1>Books</h1>
 <ul>
-    <?php foreach(filterByAuthor($books, "Jane Austen") as $book) : ?>
+    <?php foreach($filteredBooks as $book) : ?>
             <a href="<?= $book['purchaseUrl']; ?>">
                 <li><?= $book['name'] ?> by <?= $book['author'] ?></li>
             </a>
