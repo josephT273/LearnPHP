09025a7178b06c5f57f13d8f1c422751a6ff123a
josepht273 <josephtadessem@gmail.com>
2025-02-21 09:36:02 +0300

    Refactor functions by moving them to a new file and update index.php to fetch posts from the database

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
 <?php
 
-require 'controllers/functions.php';
-require 'router.php';
+require 'functions.php';
+// require 'router.php';
+
+$dsn = "mysql:host=localhost;dbname=learn_php;user=root;pass=''charset=utf8mb4";
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("SELECT * FROM posts");
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach($posts as $post) {
+    echo $post['title'] . '<br>';
+}
