a11e4354de5c25121129300d9cfcbcdb1e3e37c6
josepht273 <josephtadessem@gmail.com>
2025-03-02 21:49:26 +0300

    Implement note deletion functionality with redirect and update view for delete action

--- Core/functions.php
+++ Core/functions.php
@@ -32,3 +32,9 @@ function view($path, $attributes = [])
     extract($attributes);
     require base_path("views/{$path}");
 }
+
+function redirect($path)
+{
+    header("Location: {$path}");
+    exit();
+}
--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -6,19 +6,39 @@ use Core\Database;
 $config = require(base_path('config.php'));
 $db = new Database($config['database'], 'root', 'password');
 
-$id = $_GET['id'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $note = $db->query(
+        'SELECT * FROM notes WHERE id = :id',
+        [
+            'id' => $_GET['id']
+        ]
+    )->findOrFail();
+
+    authorize($note['user_id'] == 100);
 
 
-$note = $db->query(
-    'SELECT * FROM notes WHERE id = :id',
-    [
-        'id' => $_GET['id']
-    ]
-)->findOrFail();
+    $db->query('DELETE FROM notes WHERE id = :id', [
+        'id' => $_POST['id']
+    ]);
 
-authorize($note['user_id'] == 1);
+    redirect('/notes');
+    exit();
+} else {
 
-view('/notes/show.view.php', [
-    'heading' => "Note " . htmlspecialchars($id),
-    'note' => $note
-]);
+    $id = $_GET['id'];
+
+
+    $note = $db->query(
+        'SELECT * FROM notes WHERE id = :id',
+        [
+            'id' => $_GET['id']
+        ]
+    )->findOrFail();
+
+    authorize($note['user_id'] == 1);
+
+    view('/notes/show.view.php', [
+        'heading' => "Note " . htmlspecialchars($id),
+        'note' => $note
+    ]);
+}
