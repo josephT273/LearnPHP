a8b8aeeda9dcff00f2a29fd78db310268326cb71
josepht273 <josephtadessem@gmail.com>
2025-03-30 11:43:25 +0300

    Implement middleware handling for authentication and guest access

--- routes.php
+++ routes.php
@@ -1,15 +1,14 @@
 <?php
 
-
 $router->get('/', 'controllers/index.php');
 $router->get('/about', 'controllers/about.php');
 $router->get('/contact', 'controllers/contact.php');
-$router->get('/notes', 'controllers/notes/index.php');
+$router->get('/notes', 'controllers/notes/index.php')->only("auth");
 $router->post('/notes', 'controllers/notes/store.php');
 $router->get('/note', 'controllers/notes/show.php');
 $router->get('/note/edit', 'controllers/notes/edit.php');
 $router->delete('/note', 'controllers/notes/destroy.php');
 $router->get('/note/create', 'controllers/notes/create.php');
 $router->patch('/note', 'controllers/notes/update.php');
-$router->get('/register', 'controllers/registration/create.php');
+$router->get('/register', 'controllers/registration/create.php')->only('heloo');
 $router->post('/register', 'controllers/registration/store.php');
